3fc4b355afeca843028529c6501dc1e144858d20
José Antonio <joseapaez96@gmail.com>
2022-05-28 18:22:01 +0200

    Fraccion clase hecha

---
+++ src/fraccion.php
@@ -0,0 +1,37 @@
+<?php
+
+//Clase 1: Crea una clase Fracción con métodos para sumar, restar, multiplicar y dividir fracciones.
+
+class Fraccion {
+    private int $num1;
+    private int $num2;
+
+    public function __construct(int $num1, int $num2) {
+        $this->num1 = $num1;
+        $this->num2 = $num2;
+    }
+
+    public function getnum1() {
+        return $this->num1;
+    }
+
+    public function getnum2() {
+        return $this->num2;
+    }
+
+    public function sum(){
+        return $this->getnum1() + $this->getnum2();
+    }
+
+    public function rest(){
+        return $this->getnum1() - $this->getnum2();
+    }
+
+    public function mult(){
+        return $this->getnum1() * $this->getnum2();
+    }
+
+    public function div(){
+        return $this->getnum1() / $this->getnum2();
+    }
+}

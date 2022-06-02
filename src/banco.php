2f576c13fac2b2429f1b4be84f8d2d9fbcfb5036
Ignacio Núñez <JefaturaEstudios@itecfp.com>
2022-06-02 10:25:10 +0200

    ahora con array reduce

--- src/banco.php
+++ src/banco.php
@@ -1,20 +1,22 @@
 <?php
 namespace ITEC\PRESENCIAL\DAW\PROG\ejerciciosvarios;
-//Clase 4: Crea una clase banco. contendrá toda la lógica para incluir varios números de cuentas y sus saldos.
-// Podrá hacerse ingresos y aplicar cargos. Un método que permita obtener el saldo final de una cuenta y el total en el banco.
+//Clase 4: Crea una clase banco. contendrá toda la lógica para incluir varios números de cuentas 
+//y sus saldos. Podrá hacerse ingresos y aplicar cargos. Un método que permita obtener el 
+//saldo final de una cuenta y el total en el banco.
 
 class banco{
     private int $numcuenta;
     private int $saldo;
-    private array $cuentas;
-    private static $numcargas=0;
-    private static $numingresos=0;
+    private static array $cuentas;
+    private int $numcargas;
+    private int $numingresos;
 
     public function __construct(int $numcuenta, int $saldo) {
         $this->numcuenta = $numcuenta;
         $this->saldo = $saldo;
-        self::$numcargas=0;
-        self::$numingresos=0;
+        $this->numcargas=0;
+        $this->$numingresos=0;
+        self::$cuentas[]=$this;
     }
 
     public function createCuenta(int $numcuenta, int $saldo) {
@@ -31,26 +33,48 @@ class banco{
 
     public function ingresar(int $saldo) {
         $this->saldo += $saldo;
+        $this->numingresos++;
     }
 
     public function cargar(int $saldo) {
         $this->saldo -= $saldo;
+        $this->numcargas++;
     }
 
-    public function getnumCuentas(){
-        return count($this->cuentas);
+    public static function getnumCuentas(){
+        return count(self::$cuentas);
     }
 
     public function numcargas(){
-        return self::$numcargas;
+        return $this->numcargas;
     }
 
     public function numingresos(){
-        return self::$numingresos;
+        return $this->numingresos;
     }
 
     public function getSaldoFinalCuenta(){
-        return 
+        return $this->saldo;
+    }
+
+    public static function getSaldoBanco1(){
+        return array_reduce(
+            self::$cuentas,
+            function($suma,$cuentaactual){
+                $suma+=$cuentaactual->getSaldoFinalCuenta();
+                return $suma;
+            },
+            0
+        );
+    }
+
+    public static function getSaldoBanco2(){
+        $suma=0;
+        foreach(self::$cuentas as $cuenta){
+            $suma += $cuenta->getSaldoFinalCuenta();
+        }
+        return $suma;
+    }
 
 
         

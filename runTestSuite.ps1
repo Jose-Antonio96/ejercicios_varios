7e3d754b94df3cef08276a2e175212068f44b5d4
José Antonio <joseapaez96@gmail.com>
2022-06-04 16:16:20 +0200

    main

--- runTestSuite.ps1
+++ runTestSuite.ps1
@@ -1,3 +1,3 @@
 $date=$(Get-Date).ToString("yyyyMMddhhmmss")
 $OutputFileName=$date+"_ReportSuite.html"
-./vendor/bin/phpunit ./tests/$OutputFileName--testdox-html ./logs/tests/$OutputFileName --coverage-html ./logs/coverage
+./vendor/bin/phpunit --testdox-html ./logs/tests/$OutputFileName --coverage-html ./logs/coverages/$OutputFileName
